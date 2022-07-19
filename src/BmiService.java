7d89384bb87a0f4e48de885760e7624d36d01154
Toybear1986 <vaskorobogatov@yandex.ru>
2022-07-19 09:01:43 +0300

    "Refine code"

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
 
-    public double calculate(int m /*вес*/, double h /*рост*/) {
-            return m / (Math.pow(h/100,2));
+    public double calculate(int weight, float height) {
+        return weight / (Math.pow(height / 100, 2));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int m = 95;
-        int h = 154;
-        double bmi = service.calculate(m,h);
-        String result = String.format("%.2f",bmi);
-        System.out.println("Индекс массы тела: " + result);
+        System.out.println();
+        System.out.println("Индекс массы тела: " + String.format("%.2f", (service.calculate(95, 154))));
     }
 }
